e4821961d7aa3ca9eb52b56914e8a9a48b67902c
Aigarsss <aigars.uplejs@gmail.com>
2021-03-14 21:49:36 +0200

    added db connection

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 // phpinfo();
 
 require 'libraries/Router.php';
+require 'libraries/Database.php';
+
 
 $router = new Router();
+$db = new Database();
 
 // require 'Model.php';
 // $model = new Model();
--- libraries/Database.php
+++ libraries/Database.php
@@ -3,6 +3,33 @@
 
 class Database {
     
+    private $db_name = DB_NAME;
+    private $db_username = DB_USER;
+    private $db_server = DB_SERVER;
+    private $db_password = DB_PASSWORD;
 
+    private $dsn;
+    private $dbh;
+    private $error;
+
+
+
+    public function __construct() {
+
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        $this->dsn = "mysql:dbname=" . $this->db_name . ";host=" . $this->db_server;
+
+        try {
+            $this->dbh = new PDO($this->dsn, $this->db_username, $this->db_password, $options);
+            echo "test";
+        } catch(PDOExcepton $e) {
+            $this->error = $e->getMessage();
+            echo "connection failed: " . $this->error;
+        }
+
+    }
     
 }
